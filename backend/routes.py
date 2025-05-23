83fdf263f109380838456ccfb251c3194c1772e7
Raissa Ji <rj353@cornell.edu>
2025-05-23 01:12:47 -0400

    Add route that returns 10 random drinks each request

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from flask import Blueprint, request, render_template
 from user_services import UserServices
+import requests
+import random
 
 # defines API endpoints
 
@@ -8,20 +10,27 @@ routes = Blueprint('routes', __name__)
 
 user_services = UserServices()
 
-# @routes.route("/")
-# def intro():
-#   return render_template('index.html')
+page = random.randint(1, 63)
+url = f"https://boozeapi.com/api/v1/cocktails?page={page}"
+response = requests.get(url)
+response_json = response.json()
 
-  # message = user_services.greet()
-  # return Response(message, mimetype="text/plain")
-
-@routes.route('/', methods=['GET', 'POST']) # handle GET: user submitting name, and then POST: displaying the greeting
-def intro():
+@routes.route('/', methods=['GET', 'POST'])
+# handle GET: user submitting name, and then POST: displaying the greeting
+def get_name():
   if request.method == 'POST':
     name = request.form.get('text')
     return user_services.greet(name)
   return render_template('index.html')
 
-# url = "https://boozeapi.com/api/v1/cocktails"
-# response = requests.get(url)
-# print(response.json())
+@routes.route('/get_drinks', methods=['POST'])
+def get_ingredients():
+  user_services.ask_ingredients()
+
+  if request.method == 'POST':
+    all_drinks = list()
+
+    for i in response_json['data']:
+      all_drinks.append(i['name'])
+
+      return render_template('index.html', cocktails=all_drinks)
